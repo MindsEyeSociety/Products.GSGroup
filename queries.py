5f3a9d61774e0eaef78493dfcd6b49635b4a5927
Michael JasonSmith <mpj17@onlinegroups.net>
2007-12-05 04:33:04 +0000

    Fixing the user-post query.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSGroup/trunk%402883

--- queries.py
+++ queries.py
@@ -10,15 +10,18 @@ class GroupQuery(object):
         
     def authors_posts_in_group(self, siteId, groupId):
         pt = self.postTable
-        statement = sa.select([pt.c.user_id.distinct().label('author'), sa.func.count(pt.c.user_id).label('num_posts')])
+        cols = [pt.c.user_id.distinct(),
+                sa.func.count(pt.c.user_id).label('num_posts')]
+        statement = sa.select(cols)
         statement.append_whereclause(pt.c.site_id==siteId)
         statement.append_whereclause(pt.c.group_id==groupId)
-        statement.group_by([pt.c.user_id])
+        statement.group_by(pt.c.user_id)
 
         r = statement.execute()
         retval = []
         if r.rowcount:
             for row in r:
-                retval.append = {'author': row['author'], 'num_posts': row['num_posts']}
+                retval.append({'user_id': row['user_id'], 
+                               'num_posts': row['num_posts']})
         return retval
 
