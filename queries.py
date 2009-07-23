ff91fcdf40dbd24140556c4743107faa94546243
Alice Murphy <alice@onlinegroups.net>
2009-07-23 10:58:30 +0000

    Query to get the outstanding invitations for a group.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSGroup/trunk%405299

--- queries.py
+++ queries.py
@@ -1,4 +1,4 @@
-# Queries related to groups
+# coding=utf-8
 import sqlalchemy as sa
 
 class GroupQuery(object):
@@ -6,6 +6,7 @@ class GroupQuery(object):
     def __init__(self, context, da):
         self.context = context
         self.postTable = da.createTable('post')
+        self.userInvitationTable = da.createTable('user_group_member_invitation')
         
     def authors_posts_in_group(self, siteId, groupId):
         pt = self.postTable
@@ -24,3 +25,30 @@ class GroupQuery(object):
                                'num_posts': row['num_posts']})
         return retval
 
+    def get_current_invitations_for_group(self, siteId, groupId):
+        assert siteId
+        assert groupId
+        uit = self.userInvitationTable
+        cols = [uit.c.site_id, uit.c.group_id, uit.c.user_id,
+                uit.c.inviting_user_id, uit.c.invitation_date, 
+                uit.c.response_date]
+        s = sa.select(cols)
+        s.append_whereclause(uit.c.site_id  == siteId)
+        s.append_whereclause(uit.c.group_id  == groupId)
+        s.append_whereclause(uit.c.response_date == None)
+        s.order_by(sa.desc('invitation_date'))
+        
+        r = s.execute()
+        retval = []
+        if r.rowcount:
+            retval = [{
+              'site_id':          x['site_id'],
+              'group_id':         x['group_id'],
+              'user_id':          x['user_id'],
+              'inviting_user_id': x['inviting_user_id'],
+              'invitation_date':  x['invitation_date']} for x in r]
+
+        assert type(retval) == list
+        return retval
+    
+    
