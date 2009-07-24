460999f32715ae35c266e24f784cfb2ce70ca0ae
Alice Murphy <alice@onlinegroups.net>
2009-07-24 06:23:37 +0000

    Moved the query for getting invited members to GSGroupMember.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSGroup/trunk%405302

--- queries.py
+++ queries.py
@@ -6,7 +6,6 @@ class GroupQuery(object):
     def __init__(self, context, da):
         self.context = context
         self.postTable = da.createTable('post')
-        self.userInvitationTable = da.createTable('user_group_member_invitation')
         
     def authors_posts_in_group(self, siteId, groupId):
         pt = self.postTable
@@ -25,30 +24,5 @@ class GroupQuery(object):
                                'num_posts': row['num_posts']})
         return retval
 
-    def get_current_invitations_for_group(self, siteId, groupId):
-        assert siteId
-        assert groupId
-        uit = self.userInvitationTable
-        cols = [uit.c.site_id, uit.c.group_id, uit.c.user_id,
-                uit.c.inviting_user_id, uit.c.invitation_date, 
-                uit.c.response_date]
-        s = sa.select(cols)
-        s.append_whereclause(uit.c.site_id  == siteId)
-        s.append_whereclause(uit.c.group_id  == groupId)
-        s.append_whereclause(uit.c.response_date == None)
-        s.order_by(sa.desc('invitation_date'))
-        
-        r = s.execute()
-        retval = []
-        if r.rowcount:
-            retval = [{
-              'site_id':          x['site_id'],
-              'group_id':         x['group_id'],
-              'user_id':          x['user_id'],
-              'inviting_user_id': x['inviting_user_id'],
-              'invitation_date':  x['invitation_date']} for x in r]
 
-        assert type(retval) == list
-        return retval
-    
     
