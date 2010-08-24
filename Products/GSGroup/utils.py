1beb74412125d5aa3f2d5848bb781e72c37a127d
Richard Waid <richard@iopen.net>
2010-08-24 16:44:32 +1200

    Remove code that probably never worked :)

--- Products/GSGroup/utils.py
+++ Products/GSGroup/utils.py
@@ -1,10 +1,12 @@
+#coding: utf-8
+
 import datetime
 from Products.XWFCore.cache import SimpleCacheWithExpiry
+from AccessControl.PermissionRole import rolesForPermissionOn #@UnresolvedImport
 
 visibilityCache = SimpleCacheWithExpiry('GSGroup.utils.visibilityCache')
 visibilityCache.set_expiry_interval(datetime.timedelta(0, 60))
 
-#coding: utf-8
 PERM_ODD = 0
 PERM_ANN = 1
 PERM_GRP = 2
@@ -25,7 +27,7 @@ def get_visibility(instance):
         retval = cached_retval    
     else:
         retval = PERM_ODD
-        roles = get_roles_with_perm(instance, 'View')
+        roles = rolesForPermissionOn('View', instance)
         if (('Anonymous' in roles) and ('Authenticated' in roles)):
             retval = PERM_ANN
         elif ('GroupMember' in roles):
@@ -35,20 +37,10 @@ def get_visibility(instance):
     assert type(retval) == int
     assert retval in (PERM_ODD, PERM_ANN, PERM_GRP)
     return retval
-
     
 def clear_visibility_cache(instance):
     cache_key = get_cache_key(instance)
     visibilityCache.remove(cache_key)
-
-def get_roles_with_perm(instance, perm):
-    assert instance
-    assert hasattr(instance, 'rolesOfPermission'), \
-      u'Instance %s has no rolesOfPermission' % instance
-    retval = [r['name'] for r in instance.rolesOfPermission(perm)
-                  if r and (r['selected'] == 'SELECTED' and r['name']) ]
-    assert type(retval) == list
-    return retval
     
 def is_public(g):
     retval = (get_visibility(g) == PERM_ANN)\
