52b6fee5bda9210f651a29a3c6b0bb55855f0155
richard@iopen.net <richard@iopen.net>
2012-06-20 14:25:34 +1200

    Switching the group-visibility cache to use gs.cache.

--- Products/GSGroup/utils.py
+++ Products/GSGroup/utils.py
@@ -1,46 +1,38 @@
 #coding: utf-8
 
 import datetime
-from Products.XWFCore.cache import SimpleCacheWithExpiry
 from AccessControl.PermissionRole import rolesForPermissionOn #@UnresolvedImport
-
-visibilityCache = SimpleCacheWithExpiry('GSGroup.utils.visibilityCache')
-visibilityCache.set_expiry_interval(datetime.timedelta(0, 60))
+from gs.cache import cache, SimpleCache
 
 PERM_ODD = 0
 PERM_ANN = 1
 PERM_GRP = 2
 
-def get_cache_key(instance):
+def ck(instance):
+    instance_id = 'REPLACEME'
     if instance:
-        cache_key = str(instance.getPhysicalPath())
+        cache_key = instance_id+':'+'%'.join(instance.getPhysicalPath())
     else:
-        cache_key = 'None'
+        cache_key = instance_id+':None'
         
     assert type(cache_key) == str
     return cache_key
 
+@cache('GSGroup.utils.get_visibility', ck)
 def get_visibility(instance):
-    cache_key = get_cache_key(instance)
-    cached_retval = visibilityCache.get(cache_key)
-    if cached_retval:
-        retval = cached_retval    
-    else:
-        retval = PERM_ODD
-        roles = rolesForPermissionOn('View', instance)
-        if (('Anonymous' in roles) and ('Authenticated' in roles)):
-            retval = PERM_ANN
-        elif ('GroupMember' in roles):
-            retval = PERM_GRP
+    retval = PERM_ODD
+    roles = rolesForPermissionOn('View', instance)
+    if (('Anonymous' in roles) and ('Authenticated' in roles)):
+        retval = PERM_ANN
+    elif ('GroupMember' in roles):
+        retval = PERM_GRP
 
-        visibilityCache.add(cache_key, retval)
     assert type(retval) == int
     assert retval in (PERM_ODD, PERM_ANN, PERM_GRP)
     return retval
-    
+
 def clear_visibility_cache(instance):
-    cache_key = get_cache_key(instance)
-    visibilityCache.remove(cache_key)
+    cache = SimpleCache('GSGroup.utils.get_visibility', ck(instance))  
     
 def is_public(g):
     retval = (get_visibility(g) == PERM_ANN)\
